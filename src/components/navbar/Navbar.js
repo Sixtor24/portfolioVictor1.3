c58bcca88811b3cd9a9a5265e546047ed9b3f216
Victor <vector6971@gmail.com>
2023-02-06 17:44:42 -0400

    “ver1”

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import logo from "../../assets/images/logo.png"
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        <div>
+            <img src={logo} alt='logo' />
+        </div>
+        <div></div>
+    </div>
   )
 }
 
