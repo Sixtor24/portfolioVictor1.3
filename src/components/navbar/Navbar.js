12443cd8dee5f1c09cd1f3786ce27e4cb845d3e9
Victor <vector6971@gmail.com>
2023-02-06 18:09:02 -0400

    “ver1.1”

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,24 @@
-import React from 'react'
-import logo from "../../assets/images/logo.png"
+import React from 'react';
+import logo from "../../assets/images/logo.png";
+import { navLinksdata } from '../../constants';
 
 const Navbar = () => {
-  return (
-    <div>
-        <div>
-            <img src={logo} alt='logo' />
+    return (
+        <div className="w-full h-20 mx-auto flex justify-between items-center font-titleFont">
+            <div>
+                <img src={logo} alt='logo' />
+            </div>
+            <div>
+                <ul className="flex items-center gap-10">
+                    {navLinksdata.map(({ _id, title, link }) => (
+                            <li className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300" key={_id}>
+                                {title}
+                            </li>
+                    ))}
+                </ul>
+            </div>
         </div>
-        <div></div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
